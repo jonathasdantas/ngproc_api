012d8cd6ba8529c79299cba0b9ccd61cc6647146
Jonathas Dantas <jonathasdantas@gmail.com>
2017-01-08 11:32:54 -0300

    Judging Bodies alignment with new route

--- app/controllers/judging_bodies_controller.rb
+++ app/controllers/judging_bodies_controller.rb
@@ -1,11 +1,11 @@
 class JudgingBodiesController < ApplicationController
   # GET - ALL
   def index
-    render json: JudgingBody.all
+    render json: JudgingBody.where(district_id: params[:district_id])
   end
 
   # GET - BY ID
   def show
-    render json: JudgingBody.find(params[:id])
+    render json: JudgingBody.where(params[:id], district_id: params[:district_id])
   end
 end
